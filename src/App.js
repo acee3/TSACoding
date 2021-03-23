c04a29f313137b4a6b402bf387754ada33c0ba46
kliu23 <59066393+kliu23@users.noreply.github.com>
2021-03-22 21:15:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,119 +1,12 @@
 import React, { useState } from 'react';
-import { Text, View, Button, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Tabs from './components/Tabs';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTabs />
+      <Tabs/>
     </NavigationContainer>
   );
 }
-
-function Feed() {
-  return (
-    <View style = {styles.container}>
-      <Image source={{ uri: 'https://i.imgur.com/6jZxnwl.png' }} style={styles.logo} />
-      <Text style={styles.titleText}>
-        Brothers Construction
-      </Text>
-      <TouchableOpacity onPresss={() => alert('Hello, world!')} style = {styles.button}>
-        <Text style={styles.buttonText}>
-          Test Button
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-function Profile() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
-    </View>
-  );
-}
-
-function Notifications() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications!</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Feed"
-      tabBarOptions={{
-        activeTintColor: '#e91e63',
-      }}
-    >
-      <Tab.Screen
-        name="Feed"
-        component={Feed}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={Notifications}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: "green",
-    padding: 15,
-    borderRadius: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-  },
-  titleText: {
-    color: '#888', 
-    fontSize: 25, 
-    marginBottom: 20,
-    fontWeight: 'bold',
-  },
-});
 
